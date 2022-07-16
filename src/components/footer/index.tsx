a074a4f9324f7625c3d3cf046ea2efcfff919f17
J&G <denbaguspranata@gmail.com>
2022-07-17 01:41:31 +0700

    Update index.tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
     <footer className="flex justify-center items-center text-center pb-4">
       <p className="text-lg font-semibold">
         Dibuat dengan ♥ oleh{" "}
-        <Link href="https://github.com/haikelz">
+        <Link href="https://github.com">
           <span className="cursor-pointer text-blue-500 hover:text-blue-600 hover:underline">
-            haikelz
+            
           </span>
         </Link>
       </p>
